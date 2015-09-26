bcf939aa1c902d45f8f553ce54f35b62dec4811f
AlastairAtFanduel <alastair@fanduel.com>
2015-09-26 18:07:25 +0100

    Working on it

--- nfldata2/nfldata2/drives.py
+++ nfldata2/nfldata2/drives.py
@@ -1,8 +1,51 @@
 #List of plays
 #List of drives
 
-def parse_drive(drive_data):
-    pass
+
+#data['2015092400']['drives']['1']['result']
+#u'Safety'
+#(Pdb) data['2015092400']['drives']['1']['start']
+#{u'yrdln': u'WAS 19', u'team': u'WAS', u'qtr': 1, u'time': u'15:00'}
+#(Pdb) data['2015092400']['drives']['1']['end']
+#{u'yrdln': u'WAS 17', u'team': u'WAS', u'qtr': 1, u'time': u'12:47'}
+#
+#(Pdb) data['2015092400']['drives']['1']['postime']
+#u'2:13'
+#data['2015092400']['drives']['1']['numplays']
+#7
+#
+#data['2015092400']['drives']['1']['penyds']
+#
+## first downs?
+#(Pdb) data['2015092400']['drives']['1']['fds']
+#0
+#data['2015092400']['drives']['1']['ydsgained']
+#8
+#data['2015092400']['drives']['1']['plays'].keys()
+#[u'159', u'57', u'35', u'195', u'137', u'102', u'81']
+
+
+#---
+# Team sats
+#---
+
+
+    #game_flow:
+    #    field_position
+    #    time
+    #    team in possesion
+    #    possesion time
+    #    game_clock
+    #    a drive :)
+    #    a play
+
+
+def parse_drives(drive_data):
+    for drive_id, drive_data in sorted(drives.items()):
+        import pdb; pdb.set_trace()
+        plays = drive_data['plays']
+    
+    play_events = data['players']  #?
 
 def parse_play(play):
     pass
--- nfldata2/nfldata2/parse_data.py
+++ nfldata2/nfldata2/parse_data.py
@@ -8,6 +8,7 @@ from nfldata2.common import game_file_path
 from nfldata2.players import load_players
 from nfldata2.schedule import get_latest_schedule
 from nfldata2.grouped_stats import parse_grouped_stats
+from nfldata2.drives import parse_drives
 
 PLAYERS = load_players()
 
@@ -18,187 +19,33 @@ def get_game_data(game_id):
     return data
 
 
-
+game_nt = namedtuple('game_nt', ['home', 'away'])
+game_data_nt = namedtuple('game_data_nt', ['name', 'score', 'stats'])
 
 
 def parse_game_file(game_id):
     raw_data = get_game_data(game_id)
     data = json.loads(raw_data)[game_id]
 
-    home = self.data['home']['abbr']
-    away = self.data['away']['abbr']
+    assert self.data['qtr'] == 'Final'
 
-    home_score = int(self.data['home']['score']['T'])
-    away_score = int(self.data['away']['score']['T'])
-
-    home_stats = parse_grouped_stats(self.data['home']['stats'])
-    away_stats = parse_grouped_stats(self.data['away']['stats'])
-
-    drives = self.data['drives']
-    for drive_id, drive_data in sorted(drives.items()):
-        import pdb; pdb.set_trace()
-        plays = drive_data['plays']
+    home_data = {}
+    home_data['name'] = self.data['home']['abbr']
+    home_data['score'] = int(self.data['home']['score']['T'])
+    home_stats_dict = generate_side_stats(game.stats_home)
+    home_data['stats'] = parse_grouped_stats(self.data['home']['stats'])
     
-    play_events = data['players']  #?
+    away_data = {}
+    away_data['name'] = self.data['away']['abbr']
+    away_data['score'] = int(self.data['away']['score']['T'])
+    away_stats_dict = generate_side_stats(game.stats_home)
+    away_data['stats'] = parse_grouped_stats(self.data['away']['stats'])
+
+    game = game_nt(home=game_data_nt(*home_data*), away=game_data_nt(**away_data))
 
 
-
-    self.data['qtr']
-    self.data['clock']
-
-
-
-
-#data['2015092400']['drives']['1']['result']
-#u'Safety'
-#(Pdb) data['2015092400']['drives']['1']['start']
-#{u'yrdln': u'WAS 19', u'team': u'WAS', u'qtr': 1, u'time': u'15:00'}
-#(Pdb) data['2015092400']['drives']['1']['end']
-#{u'yrdln': u'WAS 17', u'team': u'WAS', u'qtr': 1, u'time': u'12:47'}
-#
-#(Pdb) data['2015092400']['drives']['1']['postime']
-#u'2:13'
-#data['2015092400']['drives']['1']['numplays']
-#7
-#
-#data['2015092400']['drives']['1']['penyds']
-#
-## first downs?
-#(Pdb) data['2015092400']['drives']['1']['fds']
-#0
-#data['2015092400']['drives']['1']['ydsgained']
-#8
-#data['2015092400']['drives']['1']['plays'].keys()
-#[u'159', u'57', u'35', u'195', u'137', u'102', u'81']
-
-
-#---
-# Team sats
-#---
-
-
-
-
-parse_game_file(2015092400)
-
-
-
-
-
-    #game_flow:
-    #    field_position
-    #    time
-    #    team in possesion
-    #    possesion time
-    #    game_clock
-    #    a drive :)
-    #    a play
-
-    #def is_pregame(self):
-    #    return self.qtr == 'Pregame'
- 
-    #def is_halftime(self):
-    #    return self.qtr == 'Halftime'
- 
-    #def is_final(self):
-    #    return 'final' in self.qtr.lower()
-
-
-
-def _json_plays(drive, data):
-    """
-    Takes a single JSON drive entry (data) and converts it to a list
-    of Play objects. This includes trying to resolve duplicate play
-    conflicts by only taking the first instance of a play.
-    """
-    plays = []
-    seen_ids = set()
-    seen_desc = set()  # Sometimes duplicates have different play ids...
-    for playid in map(str, sorted(map(int, data))):
-        p = data[playid]
-        desc = (p['desc'], p['time'], p['yrdln'], p['qtr'])
-        if playid in seen_ids or desc in seen_desc:
-            continue
-        seen_ids.add(playid)
-        seen_desc.add(desc)
-        plays.append(Play(drive, playid, data[playid]))
-    return plays
-
-def _json_play_players(play, data):
-    """
-    Takes a single JSON play entry (data) and converts it to an OrderedDict
-    of player statistics.
-
-    play is the instance of Play that this data is part of. It is used
-    to determine whether the player belong to the home team or not.
-    """
-    players = OrderedDict()
-    for playerid, statcats in data.iteritems():
-        if playerid == '0':
-            continue
-        for info in statcats:
-            if info['statId'] not in nflgame.statmap.idmap:
-                continue
-            if playerid not in players:
-                home = play.drive.game.is_home(info['clubcode'])
-                if home:
-                    team_name = play.drive.game.home
-                else:
-                    team_name = play.drive.game.away
-                stats = nflgame.player.PlayPlayerStats(playerid,
-                                                       info['playerName'],
-                                                       home, team_name)
-                players[playerid] = stats
-            statvals = nflgame.statmap.values(info['statId'], info['yards'])
-            players[playerid]._add_stats(statvals)
-    return players
-
-
-def _json_play_events(data):
-    """
-    Takes a single JSON play entry (data) and converts it to a list of events.
-    """
-    temp = list()
-    for playerid, statcats in data.iteritems():
-        for info in statcats:
-            if info['statId'] not in nflgame.statmap.idmap:
-                continue
-            statvals = nflgame.statmap.values(info['statId'], info['yards'])
-            statvals['playerid'] = None if playerid == '0' else playerid
-            statvals['playername'] = info['playerName'] or None
-            statvals['team'] = info['clubcode']
-            temp.append((int(info['sequence']), statvals))
-    return [t[1] for t in sorted(temp, key=lambda t: t[0])]
-
-def _json_game_player_stats(game, data):
-    """
-    Parses the 'home' and 'away' team stats and returns an OrderedDict
-    mapping player id to their total game statistics as instances of
-    nflgame.player.GamePlayerStats.
-    """
-    players = OrderedDict()
-    for team in ('home', 'away'):
-        for category in nflgame.statmap.categories:
-            if category not in data[team]['stats']:
-                continue
-            for pid, raw in data[team]['stats'][category].iteritems():
-                stats = {}
-                for k, v in raw.iteritems():
-                    if k == 'name':
-                        continue
-                    stats['%s_%s' % (category, k)] = v
-                if pid not in players:
-                    home = team == 'home'
-                    if home:
-                        team_name = game.home
-                    else:
-                        team_name = game.away
-                    players[pid] = nflgame.player.GamePlayerStats(pid,
-                                                                  raw['name'],
-                                                                  home,
-                                                                  team_name)
-                players[pid]._add_stats(stats)
-    return players
+    raw_drives = self.data['drives']
+    drives = parse_drives(raw_drives)
 
 
 def load_week(week, year=2015, phase='REG'):
@@ -210,3 +57,9 @@ def load_week(week, year=2015, phase='REG'):
             matching_games.append(game)
 
     return [parse_game_file(game.game_id) for game in matching_games]
+
+
+parse_game_file(2015092400)
+
+
+
